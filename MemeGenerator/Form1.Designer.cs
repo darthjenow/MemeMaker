774703931a62d2eace91f6dbebe42a3ae6fb280c
j3n0w <darthjenow@gmail.com>
2016-07-29 15:17:11 +0200

    Version 1.0.0
    First Release, Version 1.0.0
    Also check https://j3n0w.wordpress.com/

---
+++ MemeGenerator/Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace MemeGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBoxUpper = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBoxLower = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.groupBoxEdit = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.versionInfoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.creditsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.developedBToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.iconsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.groupBoxEdit.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBoxUpper
+            // 
+            this.textBoxUpper.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxUpper.Location = new System.Drawing.Point(75, 19);
+            this.textBoxUpper.Name = "textBoxUpper";
+            this.textBoxUpper.Size = new System.Drawing.Size(107, 20);
+            this.textBoxUpper.TabIndex = 0;
+            this.textBoxUpper.TextChanged += new System.EventHandler(this.textBoxUpper_TextChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(563, 332);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBoxLower
+            // 
+            this.textBoxLower.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxLower.Location = new System.Drawing.Point(75, 45);
+            this.textBoxLower.Name = "textBoxLower";
+            this.textBoxLower.Size = new System.Drawing.Size(107, 20);
+            this.textBoxLower.TabIndex = 2;
+            this.textBoxLower.TextChanged += new System.EventHandler(this.textBoxLower_TextChanged);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "Image Files (*.png, *.jpg)|*.png;*.jpg";
+            this.openFileDialog1.Title = "Open File";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.White;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(781, 24);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.versionInfoToolStripMenuItem,
+            this.creditsToolStripMenuItem,
+            this.developedBToolStripMenuItem});
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "Portable Network Graphic|*.png|JPEG Image|*.jpg";
+            // 
+            // groupBoxEdit
+            // 
+            this.groupBoxEdit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBoxEdit.Controls.Add(this.label2);
+            this.groupBoxEdit.Controls.Add(this.label1);
+            this.groupBoxEdit.Controls.Add(this.textBoxUpper);
+            this.groupBoxEdit.Controls.Add(this.textBoxLower);
+            this.groupBoxEdit.Enabled = false;
+            this.groupBoxEdit.Location = new System.Drawing.Point(581, 27);
+            this.groupBoxEdit.Name = "groupBoxEdit";
+            this.groupBoxEdit.Size = new System.Drawing.Size(188, 71);
+            this.groupBoxEdit.TabIndex = 7;
+            this.groupBoxEdit.TabStop = false;
+            this.groupBoxEdit.Text = "Edit Text";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Lower Text:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Upper Text:";
+            // 
+            // versionInfoToolStripMenuItem
+            // 
+            this.versionInfoToolStripMenuItem.Name = "versionInfoToolStripMenuItem";
+            this.versionInfoToolStripMenuItem.Size = new System.Drawing.Size(182, 22);
+            this.versionInfoToolStripMenuItem.Text = "Version info";
+            this.versionInfoToolStripMenuItem.Click += new System.EventHandler(this.versionInfoToolStripMenuItem_Click);
+            // 
+            // creditsToolStripMenuItem
+            // 
+            this.creditsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.iconsToolStripMenuItem});
+            this.creditsToolStripMenuItem.Name = "creditsToolStripMenuItem";
+            this.creditsToolStripMenuItem.Size = new System.Drawing.Size(182, 22);
+            this.creditsToolStripMenuItem.Text = "Credits";
+            // 
+            // developedBToolStripMenuItem
+            // 
+            this.developedBToolStripMenuItem.Name = "developedBToolStripMenuItem";
+            this.developedBToolStripMenuItem.Size = new System.Drawing.Size(182, 22);
+            this.developedBToolStripMenuItem.Text = "Developed by Jenow";
+            this.developedBToolStripMenuItem.Click += new System.EventHandler(this.developedBToolStripMenuItem_Click);
+            // 
+            // iconsToolStripMenuItem
+            // 
+            this.iconsToolStripMenuItem.Name = "iconsToolStripMenuItem";
+            this.iconsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.iconsToolStripMenuItem.Text = "Icons";
+            this.iconsToolStripMenuItem.Click += new System.EventHandler(this.iconsToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(781, 371);
+            this.Controls.Add(this.groupBoxEdit);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "MemeMaker";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBoxEdit.ResumeLayout(false);
+            this.groupBoxEdit.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxUpper;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBoxLower;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.GroupBox groupBoxEdit;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolStripMenuItem versionInfoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem creditsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem developedBToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem iconsToolStripMenuItem;
+    }
+}
+
---
+++ MemeGenerator/Form1.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MemeGenerator
+{
+    public partial class Form1 : Form
+    {
+        Font font = new Font("Impact", 200); // the font for the text
+        StringFormat stringFormat = new StringFormat();
+        String filePath = Properties.Settings.Default.filePath; // Gets the filepath from the settings
+        Bitmap img;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Gets executed if the form loads
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Sets the string format
+            stringFormat.Alignment = StringAlignment.Center;
+            stringFormat.LineAlignment = StringAlignment.Center;
+        }
+
+        /// <summary>
+        /// Gets executed if the form closes
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // Writes the path of the last selected file to the settings and saves them
+            Properties.Settings.Default.filePath = filePath;
+            Properties.Settings.Default.Save();
+        }
+
+        /// <summary>
+        /// Gets executed if exit in the toolstrip gets clicked
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        /// <summary>
+        /// Gets executed if open in the toolstrip gets clicked
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFile();
+        }
+
+        /// <summary>
+        /// Gets executed if save in the toolstrip gets clicked
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            save();
+        }
+
+        /// <summary>
+        /// Gets executed if the text in textBoxUpper changes
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void textBoxUpper_TextChanged(object sender, EventArgs e)
+        {
+            updateImg();
+        }
+
+        /// <summary>
+        /// Gets executed if the textin textBoxLower changes
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void textBoxLower_TextChanged(object sender, EventArgs e)
+        {
+            updateImg();
+        }
+
+        /// <summary>
+        /// If executed, it opens a openFile-Dialog and loads the image
+        /// </summary>
+        public void openFile()
+        {
+            if (filePath == "") // if filePath is empty, it sets it to %userprofile%
+            {
+                filePath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+            }
+
+            openFileDialog1.InitialDirectory = filePath; // sets the starting directory to the last one used
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK) // Open got clicked
+            {
+                filePath = openFileDialog1.FileName; // path gets stored
+
+                img = new Bitmap(filePath); // loads the image from the filepath
+                
+                Font fontTemp = font; // duplicates the font so it can get changed
+                font = new Font(fontTemp.Name, (int) (0.0625 * img.Height + 12.5)); // changes the font size
+
+                groupBoxEdit.Enabled = true; // enables the edit boxes
+
+                textBoxUpper.Select(); // select the textBox for the upper text
+
+                updateImg(); // updates the image
+            }
+        }
+
+        /// <summary>
+        /// If executed, it reloads the image with the text
+        /// </summary>
+        public void updateImg()
+        {
+            // reset the Bitmap and reload the image
+            img = null;
+            img = new Bitmap(filePath);
+
+            Graphics graph = Graphics.FromImage(img);
+            GraphicsPath p = new GraphicsPath();
+
+            // Adds the text in textBoxUpper
+            p.AddString(
+                textBoxUpper.Text,
+                new FontFamily("Impact"),
+                (int)FontStyle.Regular,
+                graph.DpiY * font.SizeInPoints / 72,
+                new Point((int)(0.5 * img.Width), (int)(0.1 * img.Height)),
+                stringFormat);
+
+            // Adds the text in textBoxLower
+            p.AddString(
+                textBoxLower.Text,
+                new FontFamily("Impact"),
+                (int)FontStyle.Regular,
+                graph.DpiY * font.SizeInPoints / 72,
+                new Point((int)(0.5 * img.Width), (int)(0.9 * img.Height)),
+                stringFormat);
+
+            Pen pen = new Pen(Brushes.Black, font.SizeInPoints / 10); // pen for the text outline
+            pen.Alignment = PenAlignment.Center;
+            pen.LineJoin = LineJoin.Round; // Prevents spikes on some letters
+            graph.DrawPath(pen, p); // makes the outline
+            graph.FillPath(Brushes.White, p); // fills the path
+
+            // sets the pictureBox.Image to to edited one
+            pictureBox1.Image = img;
+        }
+
+        /// <summary>
+        /// If executed, opens a safe dialog to safe the current meme
+        /// </summary>
+        public void save()
+        {
+            if (pictureBox1.Image != null) // there is something to safe
+            {
+                saveFileDialog1.InitialDirectory = filePath; // sets the starting directory to the last one
+
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK) // save got klicked
+                {
+                    pictureBox1.Image.Save(saveFileDialog1.FileName); // saves to the selected path
+                }
+            }
+            else
+            {
+                MessageBox.Show("There is nothing to safe!", "Can't safe", MessageBoxButtons.OK, MessageBoxIcon.Error); // shows an error box, that there is nothing to safe
+            }
+        }
+
+        /// <summary>
+        /// Gets executed if version info in the toolstrip gets clicked; shows version info
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void versionInfoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("The current version is " + Properties.Settings.Default.version, "Version Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        /// <summary>
+        /// Gets executed if icons-credits gets in the toolstrip gets clicked; shows the icon credits
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void iconsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult res = MessageBox.Show("All icons came from icons8.com\r\nNavigate to site?", "Icon credits", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+            
+            if (res == DialogResult.OK)
+            {
+                System.Diagnostics.Process.Start("http://icons8.com/license/");
+            }
+        }
+
+        /// <summary>
+        /// Gets executed if J3n0W in the toolbar gets clicked; shows link to blog
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void developedBToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult res = MessageBox.Show("Developed by J3n0W.\r\nGo to the blog?", "Developer info", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+
+            if (res == DialogResult.OK)
+            {
+                System.Diagnostics.Process.Start("https://j3n0w.wordpress.com/");
+            }
+        }
+    }
+}
